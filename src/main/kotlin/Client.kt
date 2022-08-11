d4a28291d0cdfbf595b7d1ff127cbb4a710c6d68
Foldster <foldsters@gmail.com>
2022-08-11 12:30:47 -0400

    untrack files in .gitignore

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -3,8 +3,10 @@ import projects.carbon
 import projects.curler
 import projects.hopper.hopper
 import projects.hopper.hopperClouds
+import projects.hopper.hopperClouds2
 import projects.raytrace.bitfield
 import projects.shader.uniformBridgeExample
+import projects.som.paletteGenerator
 
-fun main() = bitfield()
+fun main() = paletteGenerator()
 
